9e23a5edf70302d77370e173f2a66bf3d2b74c0d
Liza <44965813+lisasiurina@users.noreply.github.com>
2025-07-29 16:15:34 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 // pages
 import Home from './pages/Home';
@@ -19,22 +19,20 @@ import { TokenProvider } from './pages/TokenContext.js';
 function App() {
   return (
     <TokenProvider>
-      <Router>
-        <div style={{display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
-          <Navbar />
-          <div style={{flex: 1}}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/dao" element={<DaoHub />} />
-              <Route path="/proposals" element={<Proposals />} />
-              <Route path="/proposal/:id" element={<Proposaldetails />} />
-              <Route path="/docs" element={<Documentation />} />
-              <Route path="/company" element={<Company />} />
-            </Routes>
-          </div>
-          <Footer />
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Navbar />
+        <div style={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dao" element={<DaoHub />} />
+            <Route path="/proposals" element={<Proposals />} />
+            <Route path="/proposal/:id" element={<Proposaldetails />} />
+            <Route path="/docs" element={<Documentation />} />
+            <Route path="/company" element={<Company />} />
+          </Routes>
         </div>
-      </Router>
+        <Footer />
+      </div>
     </TokenProvider>
   );
 }
