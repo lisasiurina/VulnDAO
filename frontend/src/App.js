1aef5b65aa9fee79cf464b577752bb97a34849fc
Liza <44965813+lisasiurina@users.noreply.github.com>
2025-07-29 17:40:49 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 // pages
 import Home from './pages/Home';
 import DaoHub from './pages/DaoHub';
 import Proposals from './pages/Proposals';
-import Proposaldetails from './pages/Proposaldetails';
+import ProposalDetails from './pages/ProposalDetails';
 import Documentation from './pages/Documentation';
 import Company from './pages/Company';
 
@@ -19,20 +19,22 @@ import { TokenProvider } from './pages/TokenContext.js';
 function App() {
   return (
     <TokenProvider>
-      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-        <Navbar />
-        <div style={{ flex: 1 }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dao" element={<DaoHub />} />
-            <Route path="/proposals" element={<Proposals />} />
-            <Route path="/proposal/:id" element={<Proposaldetails />} />
-            <Route path="/docs" element={<Documentation />} />
-            <Route path="/company" element={<Company />} />
-          </Routes>
+      <HashRouter>
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+          <Navbar />
+          <div style={{ flex: 1 }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/dao" element={<DaoHub />} />
+              <Route path="/proposals" element={<Proposals />} />
+              <Route path="/proposals/:id" element={<ProposalDetails />} />
+              <Route path="/docs" element={<Documentation />} />
+              <Route path="/company" element={<Company />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </HashRouter>
     </TokenProvider>
   );
 }
