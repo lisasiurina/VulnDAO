86b123d9b6f80a50f34e1bb621c8459be7648a90
Liza <44965813+lisasiurina@users.noreply.github.com>
2025-08-20 13:44:58 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; 
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename="/VulnDAO">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <HashRouter basename="/">
+    <App />
+  </HashRouter>
 );
-
-reportWebVitals();
