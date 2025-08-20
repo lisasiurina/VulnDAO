a579bdf832cfc300d05a64a17b463514684bc4d5
Liza <44965813+lisasiurina@users.noreply.github.com>
2025-08-20 15:37:46 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,18 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
-// pages
-import Home from './pages/Home';
-import DaoHub from './pages/DaoHub';
-import Proposals from './pages/Proposals';
-import ProposalDetails from './pages/ProposalDetails';
-import Documentation from './pages/Documentation';
-import Company from './pages/Company';
+function Home() {
+  return <div style={{color:'#fff', padding:20}}>
+    <h1>Home OK</h1>
+    <p><Link to="/dao">go /dao</Link></p>
+  </div>;
+}
+function Dao() { return <div style={{color:'#fff', padding:20}}><h1>DAO OK</h1></div>; }
 
-// components
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
-// context
-import { TokenProvider } from './pages/TokenContext.js';
-
-function App() {
+export default function App() {
   return (
-    <TokenProvider>
-      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-        <Navbar />
-        <div style={{ flex: 1 }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dao" element={<DaoHub />} />
-            <Route path="/proposals" element={<Proposals />} />
-            <Route path="/proposals/:id" element={<ProposalDetails />} />
-            <Route path="/docs" element={<Documentation />} />
-            <Route path="/company" element={<Company />} />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
-    </TokenProvider>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/dao" element={<Dao/>} />
+    </Routes>
   );
 }
-
-export default App;
